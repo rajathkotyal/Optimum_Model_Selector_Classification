37db95efc73bfaf69909ecd7485751c568a44724
rajath <rajathkotyal@gmail.com>
2020-07-08 19:55:51 +0530

    edited app.py documentation added. links provided

--- app.py
+++ app.py
@@ -1,3 +1,21 @@
+'''|**********************************************************************|
+* Project           : MiniML Model Selector, Classification, Prototype 1
+*
+* Program name      : MiniML Webapp (app.py)
+*
+* Author            : Rajath_Kotyal
+*
+* Date created      : 06/07/2020
+*
+* Purpose           : To choose the best fitted Classification Model for any given dataset
+*                     in order to the reduce time costraint of developers.
+*
+* Revision History  :
+*
+*   Date             Author           Ref                Revision
+*
+|**********************************************************************|'''
+
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -21,7 +39,7 @@ def local_css(file_name):
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 def main():
-    #st.markdown("<h1 style='text-align: left; color: black;'>Blaze Model Selector</h1>", unsafe_allow_html=True)
+    #st.markdown("<h1 style='text-align: left; color: black;'>Model Selector</h1>", unsafe_allow_html=True)
     st.title("MiniML Model Selector")
     image = Image.open('mini.png')
     st.sidebar.image(image, width = 150)
@@ -33,11 +51,12 @@ def main():
     st.markdown("Where X are the **features** & Outcome is the Output **Vector Y** Containing values 0 or 1 ")
     st.markdown("-- Make sure all the values are **Integer/Float** & there are **NO** missing values.")
     st.markdown("__Sample dataset__ is provided below ✌️")
-    st.markdown('[Documentation - Read me for more info](https://github.com/rajathkotyal/Classification_Model_Selector "Click this if you need help")')
-    st.markdown('[About Me](https://www.linkedin.com/in/rajath-kotyal-210ba8157)')
+    st.markdown('[Documentation - Read this for more info](https://github.com/rajathkotyal/Classification_Model_Selector "Click this if you need help")')
+    st.markdown('[About Me](https://www.linkedin.com/in/rajathkotyal)')
+
     uploaded_file = st.file_uploader("Upload the CSV to continue", type="csv")
 
-#LOADING FILE. * CHANGE INTO USER INPUT
+#LOADING FILE.
     @st.cache(persist = True)
     def sample_load_data():
         sample_data = pd.read_csv('sampleDiabetes.csv')
@@ -54,7 +73,7 @@ def main():
     if st.checkbox("Sample dataset", False):
         st.subheader("Sample Diabetes Dataset")
         st.write(sample_df)
-
+    st.markdown('[Click here to download the Sample Dataset (For Testing)](https://drive.google.com/uc?export=download&id=10QQHW-wKlm5rKTV6DeSkuz4SRdpHr70K)')
     st.text('[ If u see a red error box below. Please upload the CSV file with the proper format ]')
 
 
@@ -80,17 +99,21 @@ def main():
             st.subheader("Confusion Matrix :")
             plot_confusion_matrix(model, x_test,y_test,display_labels = class_names)
             st.pyplot()
+            st.markdown('[Click me to know more about Confusion Matrices](https://towardsdatascience.com/understanding-confusion-matrix-a9ad42dcfd62)')
 
         if 'ROC Curve' in metrics_list:
             st.subheader("ROC curve :")
             plot_roc_curve(model, x_test,y_test)
             st.pyplot()
+            st.text('AUC - Area Under Curve -> Higher the better Accuracy')
+            st.markdown('[Click me to know more about ROC & AUC](https://towardsdatascience.com/understanding-auc-roc-curve-68b2303cc9c5)')
 
         if 'Precision Recall Curve' in metrics_list:
             st.subheader("Precision Recall Curve:")
             plot_precision_recall_curve(model, x_test,y_test)
             st.pyplot()
-
+            st.text('AP - Average Precision -> Higher the better Accuracy')
+            st.markdown('[Click me to know more about Precision Recall Curve](https://www.geeksforgeeks.org/precision-recall-curve-ml/)')
 
 #CALLING FUNCTIONS & CHOOSING CLASSIFIER
 
@@ -122,7 +145,6 @@ def main():
             plot_metrics(metrics)
 
 #LogisticRegression
-
     if classifier == 'LogisticRegression':
         st.sidebar.subheader("Select Hyperparameters")
         C = st.sidebar.number_input("C = regularisation Parameter (Lower the better)", 0.01, 10.0, step=0.01, key='C_LR')
@@ -140,8 +162,8 @@ def main():
             st.write("Recall : ", recall_score(y_test, y_pred, labels= class_names).round(2))
             plot_metrics(metrics)
 
-#RANDOM FOREST CLASSIFIER
 
+#RANDOM FOREST CLASSIFIER
     if classifier == 'RandomForestClassifier':
         st.sidebar.subheader("Select Hyperparameters")
         n_estimators = st.sidebar.number_input("The number of trees in forest",100,5000,step=10, key='n_estimators')
@@ -161,11 +183,8 @@ def main():
             plot_metrics(metrics)
 
 #GaussianNB
-
     if classifier == 'GaussianNB':
-
         metrics = st.sidebar.multiselect("Choose the metrics : ",('Confusion Matrix','ROC Curve','Precision Recall Curve'))
-
         if st.sidebar.button("Classify", key = 'classify'):
             st.subheader("GaussianNB Results : ")
             model = GaussianNB()
@@ -180,7 +199,7 @@ def main():
 #KNeighborsClassifier
     if classifier == 'KNeighborsClassifier':
         st.sidebar.subheader("Select Hyperparameters")
-        n_neighbors = st.sidebar.slider("The number of neighbors to consiider : ",1,10,step=1,key='n_neighbors')
+        n_neighbors = st.sidebar.slider("The number of neighbors to consider : ",1,10,step=1,key='n_neighbors')
         algorithm = st.sidebar.selectbox("Algorithm",("auto","ball_tree","kd_tree","brute"),key='algorithm')
         metrics = st.sidebar.multiselect("Choose the metrics : ",('Confusion Matrix','ROC Curve','Precision Recall Curve'))
 
@@ -198,11 +217,10 @@ def main():
 #Stochastic Gradient Descent
     if classifier == 'SGD_Classifier':
         st.sidebar.subheader("Default Hyperparameters Set")
-
         #alpha = st.sidebar.selectbox("Regularization Parameter: ",("0.001","0.01","0.1","0.5"),key='alpha')
         #max_iter = st.sidebar.number_input("Maximum number of iterations", 1000,5000,key='max_iter')
+        # *issue with datatype
         metrics = st.sidebar.multiselect("Choose the metrics : ",('Confusion Matrix','ROC Curve','Precision Recall Curve'))
-
         if st.sidebar.button("Classify", key = 'classify'):
             st.subheader("SGDClassifier Results : ")
             model = SGDClassifier(loss='hinge', penalty='l2', alpha=0.0001, l1_ratio=0.15, fit_intercept=True, max_iter=1000, tol=0.001, shuffle=True, verbose=0,
@@ -216,11 +234,7 @@ def main():
             st.write("Recall : ", recall_score(y_test, y_pred, labels= class_names).round(2))
             plot_metrics(metrics)
 
-#RAW DATA FUNCTION
-
-
-
-
+#External docs
 local_css("styles.css")
 
 if __name__ == "__main__" :
